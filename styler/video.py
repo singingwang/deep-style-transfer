4e2057075073f83be8cfa68f20e365f8a6f4d241
Singing <singing9339@gmail.com>
2018-02-25 11:01:55 +0800

    test-pr

---
+++ styler/video.py
@@ -0,0 +1,45 @@
+import cv2
+
+from styler.utils import resize
+
+
+class Video:
+
+    def __init__(self, path):
+        self.path = path
+        self.cap = cv2.VideoCapture(self.path)
+        self.frames = []
+
+    def __enter__(self):
+        if not self.cap.isOpened():
+            raise Exception('Cannot open video: {}'.format(self.path))
+        return self
+
+    def __len__(self):
+        return len(self.frames)
+
+    def read_frames(self, image_h, image_w):
+        '''
+        5.
+         - Read video frames from `self.cap` and collect frames into list
+         - Apply `resize()` on each frame before add it to list
+         - Also assign frames to "self" object
+         - Return your results
+        '''
+        frames = []
+        
+        while(self.cap.isOpened()):          
+            b,frame=self.cap.read() 
+            if (b == True):
+                g = resize(frame,image_h,image_w)                
+                frames.append(g)
+            else :
+                break    
+        
+        # 5-1 /5-2 Read video and collect them
+        
+        self.frames = frames  # 5-3 let object have the result
+        return self.frames  # return your results
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cap.release()
